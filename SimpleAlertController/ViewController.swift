7c5f13550ec8d3085e263e0fcd0f17061129174a
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-04-10 16:22:15 +0900

    Simple Alert Controller

--- SimpleAlertController/ViewController.swift
+++ SimpleAlertController/ViewController.swift
@@ -13,8 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        let alertController = UIAlertController(title: "카메라", message: "Take a Picture", preferredStyle: .actionSheet)
+        
+        let camAction = UIAlertAction(title: "Camera", style: .default, handler: nil)
+        
+        let libAction = UIAlertAction(title: "Library", style: .default, handler: nil)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let delAction = UIAlertAction(title: "Delete", style: .destructive, handler: nil)
+        
+        alertController.addAction(camAction)
+        alertController.addAction(cancelAction)
+        alertController.addAction(libAction)
+        alertController.addAction(delAction)
+        alertController.addAction(saveAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
 }
 
